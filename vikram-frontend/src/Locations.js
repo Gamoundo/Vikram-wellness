40dd7a557e72bf47720774ee6212c60b7f74348d
Gamoundo <akilgg@gmail.com>
2021-07-06 15:47:37 -0400

    styled the location component a little

--- vikram-frontend/src/Locations.js
+++ vikram-frontend/src/Locations.js
@@ -6,15 +6,15 @@ function Locations(props) {
    const displayLocations = (arr) => {
     return (arr.map((element) => {
         return(
-            <div className="project">
+            <div className="locations">
                 
-                <div style= {{backgroundImage: `url(${element.img})`}}>
+                <div  style= {{backgroundImage: `url(${element.img})`, backgroundRepeat: 'no-repeat', width: '100%', height: '250px'}}>
                     <h3> {element.area}</h3>
                      <p> {element.address}</p>
                      <p> {element.phone}</p>
                      <button> See on Map</button>
                 </div>
-                <div>
+                <div className="blurb">
                     <p> {element.blurb}</p>
                 </div>
             </div>
