5e78bc51e683f2e509f75e281be88eb9d884feb1
Gamoundo <akilgg@gmail.com>
2021-08-08 19:19:37 -0400

    added select options for country and states

--- vikram-frontend/src/SignupForm.js
+++ vikram-frontend/src/SignupForm.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useState, useMemo } from 'react'
+import countryList from 'react-select-country-list'
+import Select from 'react-select'
+import SelectUSState from 'react-select-us-states';
 
 function SignupForm () {
 
+    const [value, setValue] = useState('United States')
+    const options = useMemo(() => countryList().getData())
+  
+    const changeHandler = value => {
+      setValue(value)
+    }
+
+    console.log(options)
+    console.log(SelectUSState)
+    const displayCountries = (arr) => {
+      console.log(arr)
+        return(arr.map((opt) => {
+          
+          <div>
+            <option value={opt.label}>{opt.label}</option>
+          </div>
+        
+      }
+      )
+      )  
+    }
     return(
         <div >
             <form >
@@ -22,7 +46,68 @@ function SignupForm () {
                     <div>
                         <input type="text" name="city" placeholder="City" />
                     </div>
-                    
+                    <div>
+                    <Select options={options} value={value} onChange={changeHandler} placeholder={value} />
+                    <select>
+	<option value="AL">Alabama</option>
+	<option value="AK">Alaska</option>
+	<option value="AZ">Arizona</option>
+	<option value="AR">Arkansas</option>
+	<option value="CA">California</option>
+	<option value="CO">Colorado</option>
+	<option value="CT">Connecticut</option>
+	<option value="DE">Delaware</option>
+	<option value="DC">District Of Columbia</option>
+	<option value="FL">Florida</option>
+	<option value="GA">Georgia</option>
+	<option value="HI">Hawaii</option>
+	<option value="ID">Idaho</option>
+	<option value="IL">Illinois</option>
+	<option value="IN">Indiana</option>
+	<option value="IA">Iowa</option>
+	<option value="KS">Kansas</option>
+	<option value="KY">Kentucky</option>
+	<option value="LA">Louisiana</option>
+	<option value="ME">Maine</option>
+	<option value="MD">Maryland</option>
+	<option value="MA">Massachusetts</option>
+	<option value="MI">Michigan</option>
+	<option value="MN">Minnesota</option>
+	<option value="MS">Mississippi</option>
+	<option value="MO">Missouri</option>
+	<option value="MT">Montana</option>
+	<option value="NE">Nebraska</option>
+	<option value="NV">Nevada</option>
+	<option value="NH">New Hampshire</option>
+	<option value="NJ">New Jersey</option>
+	<option value="NM">New Mexico</option>
+	<option value="NY">New York</option>
+	<option value="NC">North Carolina</option>
+	<option value="ND">North Dakota</option>
+	<option value="OH">Ohio</option>
+	<option value="OK">Oklahoma</option>
+	<option value="OR">Oregon</option>
+	<option value="PA">Pennsylvania</option>
+	<option value="RI">Rhode Island</option>
+	<option value="SC">South Carolina</option>
+	<option value="SD">South Dakota</option>
+	<option value="TN">Tennessee</option>
+	<option value="TX">Texas</option>
+	<option value="UT">Utah</option>
+	<option value="VT">Vermont</option>
+	<option value="VA">Virginia</option>
+	<option value="WA">Washington</option>
+	<option value="WV">West Virginia</option>
+	<option value="WI">Wisconsin</option>
+	<option value="WY">Wyoming</option>
+    <option value="AS">American Samoa</option>
+    <option value="GU">Guam</option>
+    <option value="MP">Northern Mariana Islands</option>
+    <option value="PR">Puerto Rico</option>
+    <option value="UM">United States Minor Outlying Islands</option>
+    <option value="VI">Virgin Islands</option>
+</select>   
+                    </div>
 
 
 
