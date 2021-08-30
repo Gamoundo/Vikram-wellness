7f0ab0dd5b510e15e7cb4850aea65e678bccd734
Gamoundo <akilgg@gmail.com>
2021-08-30 15:14:34 -0400

    added to the training page

---
+++ vikram-frontend/src/In-Studio.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function In_Studio() {
+    return(
+        <div>
+            <h1> In Studio</h1>
+            <p> Meet Us In The Hot Room</p>
+        </div>
+    )
+}
+
+export default In_Studio;
--- vikram-frontend/src/Training.js
+++ vikram-frontend/src/Training.js
@@ -23,6 +23,10 @@ function Training(props) {
         <div>
             <h1>Training</h1>
             {/* {displayTraining(props.Training)} */}
+            <p> Our signature training CEO You provides training for budding entrepreneurs who want to understand what it takes to step out on their own and do the work necessary to be competitive. We also offer professional and personal development courses throughout the year. Training is held at BYW headquarters in Riverdale, Maryland. Are you interested to know when the next event will be? Email <a >info@bikramyogaworks.com</a> and let us answer all of your questions.</p>
+            <p>Click the Video below to learn more!</p>
+            <h2>Hear Testimonials from past Attendees</h2>
+            <a className="link" href= 'http://localhost:3000/contact'> Email Us for more info </a>
         </div>
     )
 }
