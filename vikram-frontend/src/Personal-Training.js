a2bf941527bfd566045505b4d8b1f2c43d815b59
Gamoundo <akilgg@gmail.com>
2021-09-03 14:45:09 -0400

    added a section to personal training

--- vikram-frontend/src/Personal-Training.js
+++ vikram-frontend/src/Personal-Training.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Personal_Training() {
    
@@ -16,6 +16,39 @@ function Personal_Training() {
           price: "$500.00"
       }
   ]
+
+const [training, settraining] = useState(
+    [
+        {
+            type: "One-On-One",
+            info: "Get the one-on-one attention that your fitness goals need. Connect and sweat in-studio or virtually with one of our Certified Personal Trainers. Our trainers are equipped with the kind of knowledge that will ensure long lasting results no matter what your fitness needs may be. Whether you goal is to build Strength and Stability, Increase endurance and agility, improving your range of motion, rest assured that you are in good hands."
+        },
+        {
+            type: "Small Group Training",
+            info: "BYW Small Group training combines one-one-one attention within the community atmosphere of group classes. Trainers are skilled to make sure that you are productively striving towards your personal fitness goals and will enjoy your training session in the company of your friends, family, and colleagues. The goal of every BYW trainer is to make sure that everyone is working productivity in their own bodies. By being able to offer numerous modifications and progressions for all kinds of movement sequences, we can assure that you will get the most band for your buck and your body during your small group session. Group training usually consists of 4-6 individuals per session. Expect to burn anywhere between 200-500 calories within the hour session."
+        }
+    ]
+)
+
+
+const displayTraining = (arr) => {
+    return (arr.map((element) => {
+        return(
+         <div className="bonuses">
+             <div>
+                <h2> {element.type}</h2>
+                <p> {element.info}</p>
+                
+             </div>
+             
+             
+         </div>
+        )
+    }
+    )
+
+    ) 
+}
 
  const displaySessions = (arr) => {
     return (arr.map((element) => {
@@ -39,6 +72,10 @@ function Personal_Training() {
     return(
         <div>
             <h1>Personal Training</h1>
+            <div style= {{backgroundImage: `url("")`, backgroundRepeat: 'no-repeat', backgroundSize: 'contain'}}>
+                {displayTraining(training)}
+                <button> Book Your Session Now</button>
+            </div>
             <h1> Private & Small Group Training Sessions</h1> 
             <div className="list">
                 <p> Session Packages</p>
